0ee4919ea9dc832ece77bc58ec579b62756b6b97
Luca Grillotti <lg4615@ic.ac.uk>
2023-07-18 10:11:08 +0100

    adding parameter_space, get_initial_parameters methods and random_key in evaluate

--- qd_task.py
+++ qd_task.py
@@ -1,5 +1,12 @@
 """Provides QDTask."""
+import abc
 from abc import ABC, abstractmethod
+from typing import Tuple, Dict
+
+import gymnasium as gymnasium
+import jax as jax
+import jax.numpy as jnp
+import numpy as np
 
 
 class QDTask(ABC):
@@ -16,21 +23,30 @@ class QDTask(ABC):
         # as we create more, it will become obvious what code and configuration
         # should be shared here.
 
-    def evaluate(self, parameters):
+    def evaluate(self,
+                 parameters: jnp.ndarray,
+                 random_key: jnp.ndarray = None,
+                 ) -> Tuple[jnp.ndarray, jnp.ndarray, Dict[str, jnp.ndarray]]:
         """Evaluates
 
         Args:
             parameters: A batch of parameters to evaluate.
+            random_key:
         Returns:
-            (batch_of_fitness, batch_of_descriptors)
+            (batch_of_fitness, batch_of_descriptors, batch of extra_data)
         """
+        # TODO(Looka): added a random_key parameter (useful for JAX tasks
+        #  requiring random numbers).
+
         # TODO(btjanaka): How should we handle extra data in the returns here?
         # There's often a lot more info that we can return from evaluate().
         # Perhaps we can do something like `info` as is done in OpenAI Gym.
+        # (Looka) : completely agreed! changed the function signature and return type
         #
         # TODO(btjanaka): We should incorporate DQD domains here -- for example,
         # what argument should we pass in if we want to compute gradients in
         # this function? What should we return if we are computing gradients?
+
 
     @property
     @abstractmethod
@@ -63,3 +79,22 @@ class QDTask(ABC):
         #
         # TODO(btjanaka): Define the return type here - string, enum, int? I
         # would probably go with an Enum since we have a limited set of choices.
+
+    @property
+    @abstractmethod
+    def parameter_space(self) -> gymnasium.spaces.Space:
+        ...
+
+    def get_initial_parameters(self,
+                               seed: int,
+                               number_parameters: int = 1,
+                               ) -> jnp.ndarray:
+        # TODO(Looka): added this method to generate random individuals
+        #  for a given task
+        parameter_space = self.parameter_space
+        parameter_space.seed(seed)
+        initial_parameters = jnp.asarray([
+            parameter_space.sample()
+            for _ in range(number_parameters)
+        ])
+        return initial_parameters
