cbf53289d38adefcfe1a27164c0df9ff20ae2a96
Luca Grillotti <lg4615@ic.ac.uk>
2023-07-21 19:20:30 +0100

    add tests metrics

---
+++ tests/tasks/discrete_metrics_tests.py
@@ -0,0 +1,56 @@
+import gymnasium
+import numpy as np
+
+from qdglue.metrics.grid_based_metrics import CVTMetricsFunctor, GridMetricsFunctor
+
+
+def test_grid_based_metrics():
+    feature_space = gymnasium.spaces.Box(low=0.0, high=1.0, shape=(2,))
+    fitness_bounds = gymnasium.spaces.Box(low=0.0, high=1.0, shape=(1,))
+    resolution = (2, 2)
+
+    calculator = GridMetricsFunctor(
+        feature_space=feature_space,
+        fitness_bounds=fitness_bounds,
+        resolution=resolution,
+        num_points_ccdf=5,
+    )
+
+    metrics = calculator.get_metrics(
+        fitnesses=np.array([0.3, 0.5, 0.751]),
+        features=np.array([[0.55, 0.55], [0.55, 0.551], [0.3, 0.3]]),
+    )
+
+    assert metrics.max_fitness == 0.751
+    assert metrics.coverage == 0.5
+    assert metrics.qd_score == 1.251
+    assert np.all(metrics.ccdf == np.array([0.5, 0.5, 0.5, 0.25, 0.0]))
+
+
+def test_cvt_based_metrics():
+    fitness_bounds = gymnasium.spaces.Box(low=0.0, high=1.0, shape=(1,))
+
+    centroids = np.array(
+        [
+            [0.1, 0.1],
+            [0.9, 0.9],
+            [0.1, 0.9],
+            [0.9, 0.1],
+        ]
+    )
+
+    calculator = CVTMetricsFunctor(
+        fitness_bounds=fitness_bounds,
+        centroids=centroids,
+        num_points_ccdf=5,
+    )
+
+    metrics = calculator.get_metrics(
+        fitnesses=np.array([0.3, 0.5, 0.751]),
+        features=np.array([[0.9, 0.9], [0.9, 0.91], [0.3, 0.3]]),
+    )
+
+    assert metrics.max_fitness == 0.751
+    assert metrics.coverage == 0.5
+    assert metrics.qd_score == 1.251
+    assert np.all(metrics.ccdf == np.array([0.5, 0.5, 0.5, 0.25, 0.0]))
